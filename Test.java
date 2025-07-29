3987f4c659484552515f2ff2835160620172e17d
Vijayprajapat17 <vkumhar1099@gmail.com>
2025-07-29 07:18:19 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,6 +6,7 @@ public class Test {
         int b=10;
         int res=a+b;
         System.out.println("the result is " +res);
+        system.out.println("this is my first time jenkiens")
     }
     public static void main(String[] args)
     {
