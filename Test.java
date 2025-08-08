670a601f940ebe7bf1e786830fa62e720d61ab0d
Vijayprajapat17 <vkumhar1099@gmail.com>
2025-08-08 07:56:49 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,7 +9,7 @@ public class Test {
         System.out.println("this is my first time jenkiens");
         System.out.println("this is a jenkins");
         System.out.println("my name is vijay");
-        system.out.println("i do my best")
+        System.out.println("i do my best");
     }
     public static void main(String[] args)
     {
